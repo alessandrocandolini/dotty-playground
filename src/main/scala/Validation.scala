10233973efaf104ed440726979e3d0b361db7295
Alessandro Candolini <alessandrocandolini@gmail.com>
2020-11-12 17:01:16 +0000

    myeither with union types

--- src/main/scala/Validation.scala
+++ src/main/scala/Validation.scala
@@ -1,18 +1,27 @@
 object TitleValidationError
 object DueDateValidationError
 
-val validateTitle : String => TitleValidationError.type | String  =  // Either[Erratum, A]
-    case "" => TitleValidationError
-    case s => s 
+val validateTitle : String => MyEither[TitleValidationError.type, String]  =  
+    case "" => MyEither.Left(TitleValidationError)
+    case s => MyEither.Right(s)
 
-val validateDate : String => DueDateValidationError.type | String = 
-  case "" => DueDateValidationError
-  case s => s 
+val validateDate : String => MyEither[DueDateValidationError.type, String] = 
+  case "" => MyEither.Left(DueDateValidationError)
+  case s => MyEither.Right(s)  
 
-val validateBoth : String => TitleValidationError.type | DueDateValidationError.type | String =
-  s => validateTitle(s) match
-    case TitleValidationError => TitleValidationError 
-    case p : String => validateDate(p)
+val validateBoth : String => String => MyEither[TitleValidationError.type | DueDateValidationError.type, (String, String) ] = 
+  t => d => for {
+    t1 <- validateTitle(t) 
+    d1 <- validateDate(d) 
+  } yield (t1,d1)
+
+def validateBothTypeInference(title : String, date : String) = 
+  for {
+    t <- validateTitle(title) 
+    s <- validateDate(date) 
+  } yield {
+    (t,s)
+  }
 
 enum MyEither[+A,+B] derives Eql:
    case Left(a : A) extends MyEither[A, Nothing]
@@ -29,45 +38,3 @@ extension[A,B,EA, EB] (ea : MyEither[EA,A])
    def flatMap(f : A => MyEither[EB, B]) : MyEither[EA|EB,B] = ea match
      case MyEither.Left(l) => MyEither.Left(l)
      case MyEither.Right(r) => f(r)
-     
-
-
-
-     /*
-      *
-      *
-      *
-   
-trait Something 
-  def doSomething
-
-class A extends Something
-  def doSomething()
-
-
-class B(a : A) extends Something 
-  override def doSomething() = a.doSomething()
-
-
-
-def simpleFlatMap[A,B,C](f : B => MyEither[A,C]) : MyEither[A,B] => MyEither[A,C] = 
-     case Left(l) => Left(l)
-     case Right(r) => f(r) 
-  
-def flatMap[A,A1 >: A, B,C](f : B => MyEither[A1,C]) : MyEither[A,B] => MyEither[A1,C] = 
-     case Left(l) => l 
-     case Right(r) => f(r) 
-
-extension[A,B,C] (e : MyEither[A,B]):
-   def map(f : B => C) : MyEither[A,C] = e match {
-       case Right(r) => Right[A,C](f(r))
-       case Left(l) => Left[A,C](l)
-   }
-
-
-extension[A,B,C] (e : MyEither[A,B]):
-   def flatMapSimple(f : B => MyEither[A,C]) : MyEither[A,C] = e match {
-       case Right(r) => f(r) 
-       case Left(l) => Left[A,C](l)
-   }
-*/
